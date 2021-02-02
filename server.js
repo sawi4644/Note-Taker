97ad8baedd15d46b7c3028bdaee66975e0463ad9
Sara Wilson <sarakatewilson98@gmail.com>
2021-02-01 20:34:11 -0500

    adding basic html router

--- server.js
+++ server.js
@@ -1,15 +1,28 @@
 const express= require('express');
+const fs= require('fs');
+const path= require('path');
 const app = express();
 const PORT= process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+//Serve a public folder
+app.use(express.static("public"));
 
-app.use(express.static("public"))
-
-require('./routes/htmlRoutes')(app);
-require('./routes/apiRoutes')(app);
 
 app.listen(PORT,()=>{
     console.log(`Server listening on http://localhost:${PORT}`)
 })
+//Storing read/write Note variables to make it easier to reference later on
+// HTML ROUTER
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+// API ROUTE
+// ADD NOTE
+// Setup the /api/notes POST route
+// DELETE NOTE
